6732e6fe4e2ed010766e5564ee396a4ec2f15c8a
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-08 21:34:50 -0400

    create items added

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,18 @@
+class ItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
+  def index
+    items = Item.all
+    render json: items
+  end
+
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: 201
+  end
+
+  private
+  def item_params
+    params.permit(:item_type, :name, :color, :occasion, :user_id)
+  end
+end
