62cbb15c5c9e64e26336eac3fb4c733cb773cca8
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-09 10:36:05 -0400

    form built

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import useEffect from 'react'
+
 
 function App() {
+  const handleItemSubmit = async () => {
+    let form = new FormData(document.querySelector('#item-form'))
+    let req = await fetch('/items', {
+      method: 'POST',
+      body: form
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>ADD NEW ITEM</h2>
+      <form id="item-form" onSubmit={(e) => {
+        e.preventDefault()
+        handleItemSubmit()
+      }}>
+        <input type="text" name="item_type" placeholder="Item type"/>
+        <input type="text" name="name" placeholder="Item name"/>
+        <input type="text" name="color" placeholder="Color"/>
+        <input type="text" name="occasion" placeholder="Occasion"/>
+        <input type="text" name="user_id" placeholder="user ID"/>
+        {/* <%= form.file_field :image %> */}
+        <input type="file" data-direct-upload-url="<%= rails_direct_uploads_url %>" />
+        <input type="submit"/>
+      </form>
     </div>
   );
 }
