614f5f889f0e386854882879453403b8191b9f54
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-11 09:51:01 -0400

    updated route - tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :create, :show]
+  resources :users, only: [:show, :create]
 
-  get '/:user_id/items', to: 'users#my_items'
+  get 'users/:user_id/items', to: 'users#my_items'
   
   # get '/users/:user_id/items', to: "items#mine"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
