0055b64452aa8fce78691ef330523da5fee50130
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-13 10:08:26 -0400

    speclling correction

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -2,7 +2,13 @@ import React from "react";
 import { useEffect } from 'react'
 
 function ItemCard({item}) {
-  const hanldleDelete = () => {
+  
+  const [itemType, setItemType] = useState("")
+  const [itemName, setItemName] = useState("")
+  const [itemColor, setItemColor] = useState("")
+  const [occasion, setOccasion] = useState("")
+
+  const handleDelete = () => {
    // alert('Delete click')
     let req = fetch(`/items/${item.id}`, {
       method: "DELETE"
@@ -19,11 +25,10 @@ function ItemCard({item}) {
     let itemEdit = new FormData(document.querySelector('#edit-item'))
     let req = await fetch(`/items/${item.id}`, {
       method: "PATCH",
-      // headers: {
-      //   "Content-Type": "application/json",
-      // },
-      body: itemEdit
-    })      // alert('I been submitted! =>', itemEdit)
+      headers: {"Content-Type" : "application/json"},
+      body : JSON.stringify(newUser)
+            }
+    )      // alert('I been submitted! =>', itemEdit)
   }
 
   return(
@@ -34,7 +39,7 @@ function ItemCard({item}) {
       <br/>
       <button onClick={(e) => {
         e.preventDefault()
-        hanldleDelete()
+        handleDelete()
       }}>Delete</button>
 
       <form id="edit-item" onSubmit={(e) => {
@@ -44,7 +49,7 @@ function ItemCard({item}) {
         <input type="text" name="item_type[item_type]" placeholder="Item type"/>
         <input type="text" name="name[name]" placeholder="Item name"/>
         <input type="text" name="color[color]" placeholder="Color"/>
-        <input type="text" name="occasion[occasiov]" placeholder="Occasion"/>
+        <input type="text" name="occasion[occasion]" placeholder="Occasion"/>
           <input type="submit"/>
       </form>
     </div>)
