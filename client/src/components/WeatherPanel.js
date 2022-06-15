f7f32da68e7ee3ecac6bdcc8cc8bb61773dd2529
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-14 21:09:18 -0400

    bassic display keyblock

--- client/src/components/WeatherPanel.js
+++ client/src/components/WeatherPanel.js
@@ -6,7 +6,7 @@ function WeatherPanel() {
     useEffect(() => {
         (async ()=> {
             // change city with filter
-          let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manhattan&units=metric&appid=${MYAPIKEY}`)
+          let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manhattan&units=metric&appid=`)
         //   let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${MYAPIKEY}`)
           let res = await req.json()
           console.log("Results => ", res)
@@ -15,7 +15,13 @@ function WeatherPanel() {
       }, [])
       console.log("Weather data", weatherData)
     return (
-        <div></div>
+        <div>
+            <h2>Today in {weatherData.name}</h2>
+            <h3>Cloud: {weatherData.clouds.all}</h3>
+            <h3>Max: {weatherData.main.temp_max}f</h3>
+            <h3>Min: {weatherData.name}</h3>
+            <h3>Humidity: {weatherData.main.humidity}%</h3>
+        </div>
     )
 }
 
