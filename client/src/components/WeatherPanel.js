32471c968bd049d773b78451c2f93fc79886766a
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-14 21:33:49 -0400

    got some lil icons but my key died for the night

--- client/src/components/WeatherPanel.js
+++ client/src/components/WeatherPanel.js
@@ -3,24 +3,39 @@ import { useEffect, useState } from 'react';
 function WeatherPanel() {
     const [weatherData, setWeatherData] = useState([])
 
-    useEffect(() => {
-        (async ()=> {
-            // change city with filter
-          let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manhattan&units=metric&appid=`)
-        //   let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${MYAPIKEY}`)
-          let res = await req.json()
-          console.log("Results => ", res)
-          setWeatherData(res)
-        })()
-      }, [])
-      console.log("Weather data", weatherData)
+    // useEffect(() => {
+    //     (async ()=> {
+    //         // change city with filter
+    //       let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manhattan&units=metric&appid=MYAPIKEYYY`)
+    //     //   let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${MYAPIKEY}`)
+    //       let res = await req.json()
+    //       console.log("Results => ", res)
+    //       setWeatherData(res)
+    //     })()
+    //   }, [])
+    //   console.log("Weather data", weatherData)
     return (
-        <div>
-            <h2>Today in {weatherData.name}</h2>
-            <h3>Cloud: {weatherData.clouds.all}</h3>
-            <h3>Max: {weatherData.main.temp_max}f</h3>
-            <h3>Min: {weatherData.name}</h3>
-            <h3>Humidity: {weatherData.main.humidity}%</h3>
+        <div className="weather-panel">
+            <h2>Today in 
+                {/* {weatherData.name} */}
+            </h2>
+            <h3>
+                <i class="fa-solid fa-cloud"></i>
+                {/* {weatherData.clouds.all} */}
+            </h3>
+            <h3>
+                <i class="fa-solid fa-temperature-high"></i>
+                {/* {weatherData.main.temp_max}f */}
+            </h3>
+            <h3>
+                <i class="fa-solid fa-temperature-low"></i>
+            {/* {weatherData.name} */}
+            </h3>
+            <h3><i class="fa-solid fa-droplet"></i>
+                {/* {weatherData.main.humidity}% */}
+            </h3>
+            <h3><i class="fa-solid fa-umbrella"></i></h3>
+            <input type="text" placeholder="Change city..."/>
         </div>
     )
 }
