7ac93668b77e8f64b98f2be77ae92dfa01cc8423
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-14 21:01:49 -0400

    basic syntax working keyblock

--- client/src/components/WeatherPanel.js
+++ client/src/components/WeatherPanel.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+
 function WeatherPanel() {
-    return <div> I will house the weather panel</div>
+    const [weatherData, setWeatherData] = useState([])
+
+    useEffect(() => {
+        (async ()=> {
+            // change city with filter
+          let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manhattan&units=metric&appid=${MYAPIKEY}`)
+        //   let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${MYAPIKEY}`)
+          let res = await req.json()
+          console.log("Results => ", res)
+          setWeatherData(res)
+        })()
+      }, [])
+      console.log("Weather data", weatherData)
+    return (
+        <div></div>
+    )
 }
 
 export default WeatherPanel;
