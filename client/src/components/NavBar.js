c11d075a56c2df2403cbe552a299be05af90f125
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-15 11:47:04 -0400

    key hidden successfully

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,22 +2,21 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
     return (
-        // <h1>HERE LIVES THE NAVBAR</h1>
-        <span>
-            <div className="nav-left">
-            <NavLink to="/" exact>
-            <i class="fa-solid fa-house-chimney"></i>
-                {/* to style me see codealong module */}
-            </NavLink>
-            <NavLink to="/dresser" exact>
-            <i class="fa-solid fa-shirt"></i>
-            </NavLink>
-            <NavLink to="/outfits" exact>
-            <i class="fa-solid fa-user-tie"></i>
-            </NavLink>
-            </div>
+        <span className="navbar">
             <div className="nav-right">
-                <h1><i>DRESSER</i></h1>
+            <h1><i>DRESSER</i></h1>
+            </div>
+            <div className="nav-left">
+                <NavLink to="/" exact>
+                    <i class="fa-solid fa-house-chimney fa-4x"></i>
+                    {/* to style me see codealong module */}
+                </NavLink>
+                <NavLink to="/dresser" exact>
+                    <i class="fa-solid fa-shirt fa-4x"></i>
+                </NavLink>
+                <NavLink to="/outfits" exact>
+                    <i class="fa-solid fa-user-tie fa-4x"></i>
+                </NavLink>
             </div>
         </span>
     )
--- client/src/components/WeatherPanel.js
+++ client/src/components/WeatherPanel.js
@@ -3,17 +3,18 @@ import { useEffect, useState } from 'react';
 function WeatherPanel() {
     const [weatherData, setWeatherData] = useState([])
 
-    // useEffect(() => {
-    //     (async ()=> {
-    //         // change city with filter
-    //       let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manhattan&units=imperial&appid=MYAPIKEYYY`)
-    //     //   let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${MYAPIKEY}`)
-    //       let res = await req.json()
-    //       console.log("Results => ", res)
-    //       setWeatherData(res)
-    //     })()
-    //   }, [])
-    //   console.log("Weather data", weatherData)
+    useEffect(() => {
+        (async ()=> {
+            // change city with filter
+          let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manhattan&units=imperial&appid=${process.env.REACT_APP_API_KEY_PRIMARY}`)
+
+        //   let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${MYAPIKEY}`)
+          let res = await req.json()
+          console.log("Results => ", res)
+          setWeatherData(res)
+        })()
+      }, [])
+      console.log("Weather data", weatherData)
     return (
         <div className="weather-panel">
             <h2>Today in 
