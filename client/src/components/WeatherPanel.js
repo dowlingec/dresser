e17779ace05a45ef33c87c27f658c223fab2b1af
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-22 13:56:03 -0400

    city chnge buttons added

--- client/src/components/WeatherPanel.js
+++ client/src/components/WeatherPanel.js
@@ -1,21 +1,20 @@
 import { useEffect, useState } from 'react';
 
 function WeatherPanel({ weatherData, setWeatherData }) {
-    const [city, setCityName] = useState("")
+    const [city, setCity] = useState("Manhattan")
 
-    // useEffect(() => {
-    //     (async ()=> {
+    useEffect(() => {
+        (async ()=> {
             
-    //         let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manhattan&units=imperial&appid=${process.env.REACT_APP_API_KEY_PRIMARY}`)
-    //         // change city with filter
-    //         //   let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${MYAPIKEY}`)
-    //         let res = await req.json()
-    //         console.log("Results => ", res)
-    //         setWeatherData(res)
-    //     })()
-    //   }, [])
-      console.log("Weather data", weatherData)
-
+            let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.REACT_APP_API_KEY_PRIMARY}`)
+            // change city with filter
+            //   let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${MYAPIKEY}`)
+            let res = await req.json()
+            console.log("Results => ", res)
+            setWeatherData(res)
+        })()
+      }, [])
+    
     return (
         <div className="weather-panel">
             <h2>Today in
@@ -33,8 +32,28 @@ function WeatherPanel({ weatherData, setWeatherData }) {
             <h3>
                 {/* {weatherData.main.humidity}% */}
             </h3>
+            Hong Kong
+{/* Bangkok
+Macau
+Singapore
+Paris
+Dubai
+New York City
+Kuala Lumpur
+Istanbul */}
+        <div className='button-box'><button onClick={()=>{
+                setCity('Berlin')
+            }}>Berlin</button>
+            <button onClick={()=>{
+                setCity('London')
+            }}>London</button>
+            <button onClick={()=>{
+                setCity('LosAngeles')
+            }}>Los Angeles</button>
+            <button onClick={()=>{
+                setCity('Toronto')
+            }}>Toronto</button></div>
             
-            <input type="text" placeholder="Change city..."/>
         </div>
     )
 }
