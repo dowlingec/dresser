260703b6297a9c6135ce1fb010ca71fb60910034
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-23 10:12:31 -0400

    basic styling complete

--- client/src/components/outfits/MyOutfits.js
+++ client/src/components/outfits/MyOutfits.js
@@ -2,37 +2,37 @@ import { useState, useEffect } from "react";
 import OutfitCard from './OutfitCard';
 
 function MyOutfits() {
-    // const [loading, setLoading] = useState(false);
-    const [outfitArray, setOutfitArray] = useState([])
-    // const [deleteOutfit, setDeleteOutfit] = useState(1)
+  // const [loading, setLoading] = useState(false);
+  const [outfitArray, setOutfitArray] = useState([])
+  const [deleteOutfit, setDeleteOutfit] = useState(1)
   
 
-    useEffect(() => {
-        (async ()=> {
-          // setLoading(true)
-            // change fetch to outfits once built
-          let req = await fetch("/users/1/outfits")
-          let res = await req.json()
-          console.log("Outfit results => ", res)
-          setOutfitArray(res)
-          // setLoading(false)
-        })()
-    }, [])
+  useEffect(() => {
+    (async ()=> {
+      // setLoading(true)
+      // change fetch to outfits once built
+      let req = await fetch("/users/1/outfits")
+      let res = await req.json()
+      console.log("Outfit results => ", res)
+      setOutfitArray(res)
+      // setLoading(false)
+    })()
+  }, [deleteOutfit])
     // console.log("New array using state", outfitArray)
     // if (loading) {
     //   return <p>Outfits loading...</p>;
     // }
 
     return (
-      <div>
-        <h1>My Outfits!</h1>
+      <div className="my-outfits">
+        {/* <h1>My Outfits!</h1> */}
         {/* <button onClick={() => {setFormVisible(true) }}><i class="fa-solid fa-square-plus fa-4x"></i></button> */}
         {/* # Currently hardcoded to user 1 */}
         {outfitArray.map((el) => {
-            return (<OutfitCard outfit={el}
-              // setDeleteOutfit={setDeleteOutfit} deleteOutfit={deleteOutfit}
-            />)
-          })}
+          return (<OutfitCard outfit={el}
+          setDeleteOutfit={setDeleteOutfit} deleteOutfit={deleteOutfit}
+          />)
+        })}
   </div>)
 }
 
--- client/src/components/outfits/OutfitCard.js
+++ client/src/components/outfits/OutfitCard.js
@@ -1,36 +1,42 @@
 import React from 'react';
 
-function OutfitCard({ outfit }) {
-  // setDeleteOutfit, deleteOutfit 
-  // const handleDelete = () => {
-  //     // alert('Delete click')
-  //      let req = fetch(`/outfits/${outfit.id}`, {
-  //        method: "DELETE"
-  //      })
-  //      setDeleteOutfit(deleteOutfit + 1)
-  // }
+function OutfitCard({ outfit, setDeleteOutfit, deleteOutfit }) {
+   
+  const handleDelete = () => {
+      // alert('Delete click')
+       let req = fetch(`/outfits/${outfit.id}`, {
+         method: "DELETE"
+       })
+       setDeleteOutfit(deleteOutfit + 1)
+  }
   console.log(outfit)
   return(
-      <div className="outfit">
+      <div className="outfit-card">
         {/* <img id="outfit-image" src={outfit.image.url} alt={outfit.color + ' ' + outfit.occasion + ' ' + outfit.name}/> */}
         {/* <h3>Description: </h3> */}
+        <div className="outfit-text">
         <h1>{outfit.details}</h1>
         <h2>Temperature: {outfit.temp_f}°f</h2>
         <h2>Humidity: {outfit.humidity_percent}%</h2>
         <br/>
+        </div>
+        
         {/* <button onClick={() => {setVisible(true) }}>Edit</button>
         <EditModal outfit={outfit} setVisible={setVisible} isVisible={isVisible}/> */}
-        {/* <button onClick={(e) => {
+        <button onClick={(e) => {
           e.preventDefault()
           handleDelete()
-        }} title="Delete Outfit"><i class="fa-solid fa-trash fa-3x"></i></button> */}
-        {/* {outfit.items.map((item) => {
+        }} title="Delete Outfit"><i class="fa-solid fa-trash fa-3x"></i></button>
+        
+      <div className="outfit-pics">
+        {outfit.items.map((item) => {
           return(
-            <img id="item-image" src={item.image.url} alt={item.color + ' ' + item.occasion + ' ' + item.name}/>
-            )
-          })} */}
+            <img id="outfit-image" src={item.image.url} alt={item.color + ' ' + item.occasion + ' ' + item.name}/>
+          )
+        })}
+      </div>
            
-      </div>)
+    </div>)
 }
 
 export default OutfitCard;
