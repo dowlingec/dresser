bcb9270d548eab4d43819ac07548b82245482922
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-22 20:58:12 -0400

    update sizing AGAIN

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,14 +8,14 @@ function NavBar() {
             {/* </div> */}
             {/* <div className="nav-left"> */}
                 <img src="/logoSmall.png" style={{ height: `100%`, marginLeft: `-2%`}}></img>
-                <NavLink to="/" exact>
+                <NavLink activeClassName="active" to="/" exact>
                     <i class="fa-solid fa-house-chimney fa-4x"></i>
                     {/* to style me see codealong module */}
                 </NavLink>
-                <NavLink to="/dresser" exact>
+                <NavLink activeClassName="active" to="/dresser" exact>
                     <i class="fa-solid fa-shirt fa-4x"></i>
                 </NavLink>
-                <NavLink to="/outfits" exact>
+                <NavLink activeClassName="active" to="/outfits" exact>
                     <i class="fa-solid fa-user-tie fa-4x"></i>
                 </NavLink>
             {/* </div> */}
